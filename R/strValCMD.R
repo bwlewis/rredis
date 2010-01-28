76203e41a2f20237ed45a79e58736260eaa1e381
Pat Shields <yeoldefortran@gmail.com>
2010-01-28 16:08:29 -0500

    Moving things around as per dd3d07e1de6eb298eb121a0c034dc31d5518556c

--- R/strValCMD.R
+++ R/strValCMD.R
@@ -1,3 +1,20 @@
+redisGet <- function(key) {
+  msg <- paste('GET ',key,'\r\n',sep='')
+  sendCmd(msg)
+}
+
+redisSet <- function(key, value) {
+  if(!is.raw(value)) value <- serialize(value,ascii=FALSE,connection=NULL)
+  msg <- paste('SET ',key,' ',length(value),'\r\n',sep='')
+  sendCmd(msg,value)
+}
+
+redisGetSet <- function(key, value) {
+  if(!is.raw(value)) value <- serialize(value,ascii=FALSE,connection=NULL)
+  msg <- paste('GETSET ',key,' ',length(value),'\r\n',sep='')
+  sendCmd(msg,value)
+}
+
 redisMGet <- function(keys) {
   msg <- paste('MGET ',paste(keys, collapse=' '),'\r\n',sep='')
   sendCmd(msg)
