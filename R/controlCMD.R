24a4b4ddfaed7b9d0a6a93f1f6d64cfdcf6ecdc1
B. W. Lewis <bwaynelewis@gmail.com>
2011-03-16 22:37:58 -0400

    R/Windows blocking connection readBin issue

--- R/controlCMD.R
+++ R/controlCMD.R
@@ -8,9 +8,10 @@ function(host='localhost', port=6379, returnRef=FALSE)
   else if(!isOpen(.redisEnv$con)) connect <- TRUE
   if(connect)
    {
-# XXX R Windows suffers from a serious problem affecting non-blocking
-# connections and readBin with raw data. The unfortunate hack solution
-# is to force blocking connections on Windows systems.
+# R Windows appears to suffer from a serious problem affecting non-blocking
+# connections and readBin with raw data, see:
+# http://www.mail-archive.com/r-devel@r-project.org/msg16420.html.
+# we force blocking connections on Windows systems.
     if(Sys.info()[[1]] == "Windows")
       con <- socketConnection(host, port, open='a+b', blocking=TRUE)
     else
