5fa5288023833d673b05e84267b0356487e0efc9
B. W. Lewis <blewis@illposed.net>
2014-04-03 09:50:55 -0400

    Fixed a BUG in redisEval that prevented more than one key from being used

--- R/scriptCMD.R
+++ R/scriptCMD.R
@@ -3,8 +3,6 @@ redisEval <- function(script, keys=vector("list",0), SHA=FALSE, ...)
   if(!is.list(keys)) keys = list(keys)
   numkeys = length(keys)
   if(numkeys>0) keys = as.character(keys)
-  if(SHA)
-    redisCmd("EVALSHA",script,as.character(numkeys),keys,...)
-  else
-    redisCmd("EVAL",script,as.character(numkeys),keys,...)
+  CMD = ifelse(SHA,"EVALSHA","EVAL")
+  do.call("redisCmd", args=c(list(script, as.character(numkeys)),keys,list(...)))
 }
